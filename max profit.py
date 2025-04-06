4de0a10b81b1c54297784cd9c70e744a67a8b472
petros <petrosnigault@gmail.com>
2025-04-06 20:01:29 +0300

    Checks if the values are correct Less Operations

--- max profit.py
+++ max profit.py
@@ -6,22 +6,17 @@ while possible == False:
     P1 = int(input("Give the P of the first equation: "))
     QD2 = int(input("Give the QD of the second equation: "))
     P2 = int(input("Give the P of the second equation: "))
-    if QD1 != 0 and P1 != 0 and QD2 != 0 and P2 != 0 and P2 != P1:
+    b = (QD2 - QD1) / (P2 - P1) #calculate b
+    a = QD1 - P1 * b #calculate a
+    if QD1 != 0 and P1 != 0 and QD2 != 0 and P2 != 0 and P2 != P1 and (QD2 - QD1) / (P2 - P1) !=0 and QD1 - P1 * b != 0:
         possible = True
     else:
-        print("A value you have provided is 0, which is impossible. Give the correct values.\n")
-
-b = (QD2 - QD1) / (P2 - P1)
-a = QD1 - P1 * b
+        print("The values you have provided are impossible. Give the correct values.\n")
 
 print(f"The equation is: QD = {a}{b}P.\n")
 
-QDtemp = QD1
-Ptemp = P1
-Pmax = a / b
-QDmax = a
-QDm = QDmax / 2
-Pm = Pmax / 2
+QDm = a / 2
+Pm = (a / b) / 2
 if Pm < 0:
     Pm *= -1
 print(f"Profit is maximized at QD = {QDm} and P = {Pm}.")
